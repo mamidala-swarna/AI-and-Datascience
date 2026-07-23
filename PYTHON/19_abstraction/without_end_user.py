ad82b705de31e7bb2c187108f39e8934f72e1246
mamidala-swarna <swarnamamidala8gmail.com>
2026-07-23 18:27:10 +0530

    Abstraction

---
+++ PYTHON/19_abstraction/without_end_user.py
@@ -0,0 +1,11 @@
+#Without Abstraction End User
+from without_two import Dell,Lenovo
+
+#End User Buying Dell Laptop
+print("=" * 50)
+print("     customer Buying Dell Laptop")
+print("="  *50)
+
+dellObject=Dell()
+dellObject.should_have_processor()
+dellObject.should_have_ram()
